e7060f0416cfac7615ca571148e7d37e57f3b167
vinicius.caldeira@stone.com.br <M@more123>
2021-07-26 23:07:38 -0300

    2 - work queues

---
+++ work-queues/NewTask/NewTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace NewTask
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "admin", Password = "admin2017" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel()) {
+                channel.QueueDeclare(queue: "task_queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+                var message = GetMessage(args);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                                    routingKey: "task_queue",
+                                    basicProperties: properties,
+                                    body: body);
+
+                Console.WriteLine(" [x] sent {0}", message);
+            }
+
+            System.Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
---
+++ work-queues/Worker/Worker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "admin", Password = "admin2017" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel()){
+                channel.QueueDeclare(queue: "task_queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                System.Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (sender, ea) => {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    System.Console.WriteLine(" [x] Received {0}", message);
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    System.Console.WriteLine(" [x] Done");
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+
+                channel.BasicConsume(queue: "task_queue",
+                autoAck: false,
+                consumer: consumer);
+
+                System.Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
