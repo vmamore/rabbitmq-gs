1f1910d50023fbfe1f9917bcc3a03cbd70c8ed4f
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-07-28 00:33:24 -0400

    3 - publish and subscribe

---
+++ publish-subscribe/EmitLog/EmitLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace EmitLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "admin", Password = "admin2017" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("logs", type: ExchangeType.Fanout);
+
+                var message =  GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs",
+                "",
+                basicProperties: null,
+                body);
+                System.Console.WriteLine(" [x] sent {0}", message);
+            }
+
+            System.Console.WriteLine(" Press [enter] to exit");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args) {
+            return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World");
+        }
+    }
+}
---
+++ publish-subscribe/ReceiveLog/ReceiveLog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "admin", Password = "admin2017" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("logs", type: ExchangeType.Fanout);
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queueName,
+                    "logs",
+                    "");
+                System.Console.WriteLine(" [*] Waiting for logs");
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) => {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    System.Console.WriteLine(" [x] {0}", message);
+                };
+                channel.BasicConsume(queueName, true, consumer);
+
+                System.Console.WriteLine("Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
